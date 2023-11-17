3e5dd6948bc4c4dc8486cfc298b62e367a61ceb8
Justin Lafferty <justin.lafferty@kw.com>
2023-11-16 20:27:24 -0500

    parsing the city

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Get the forecast for a city
+    Forecast(ForecastArgs),
+}
+
+#[derive(Args)]
+struct ForecastArgs {
+    city: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Forecast(forecast) => {
+            println!("You entered city: {:?}", forecast.city)
+        }
+    }
 }
