ee4bb26d8b4c323d7c8fc98e7aeeb8086e8d24f7
Justin Lafferty <justin.lafferty@kw.com>
2023-11-16 21:20:58 -0500

    making the request for locations

--- src/forecast/geolocation.rs
+++ src/forecast/geolocation.rs
@@ -1,3 +1,14 @@
-pub fn get_matching_locations(location: &String) {
-    println!("You passed in location: {}", location);
+use anyhow;
+use reqwest;
+
+pub fn get_matching_locations(location: &String) -> Result<(), anyhow::Error> {
+    let url = format!(
+        "https://geocoding-api.open-meteo.com/v1/search?name={}",
+        location
+    );
+
+    let body = reqwest::blocking::get(url)?.text()?;
+    println!("body = {:?}", body);
+
+    return Ok(());
 }
