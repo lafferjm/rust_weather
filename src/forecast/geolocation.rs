00a227de3b1fe71eb71d7e2686e413c17f5d5a8d
Justin Lafferty <justin.lafferty@kw.com>
2023-11-16 21:52:07 -0500

    filtering locations

--- src/forecast/geolocation.rs
+++ src/forecast/geolocation.rs
@@ -1,14 +1,34 @@
 use anyhow;
 use reqwest;
+use serde::Deserialize;
 
-pub fn get_matching_locations(location: &String) -> Result<(), anyhow::Error> {
+#[derive(Deserialize)]
+pub struct GeolocationResponse {
+    pub results: Vec<Locations>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Locations {
+    #[serde(alias = "name")]
+    pub city: String,
+    #[serde(alias = "admin1")]
+    pub state: String,
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+pub fn get_matching_locations(location: &String) -> Result<Vec<Locations>, anyhow::Error> {
     let url = format!(
         "https://geocoding-api.open-meteo.com/v1/search?name={}",
         location
     );
 
-    let body = reqwest::blocking::get(url)?.text()?;
-    println!("body = {:?}", body);
+    let response = reqwest::blocking::get(&url)?.json::<GeolocationResponse>()?;
+    let filtered_results = response
+        .results
+        .into_iter()
+        .filter(|loc| loc.city.to_lowercase() == location.to_lowercase())
+        .collect();
 
-    return Ok(());
+    return Ok(filtered_results);
 }
